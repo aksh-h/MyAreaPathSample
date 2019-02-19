7c178a6af0048351df53c5a11b2c72f4a46ce6cb
Akshay H <h.akshay@outlook.com>
2019-02-19 17:16:42 +0530

    addede repo area map class

---
+++ AreaPathSecuritySample/AreaPathSecuritySample/RepoAreaMap.cs
@@ -0,0 +1,9 @@
+﻿namespace AreaPathSecuritySample
+{
+    public class RepoAreaMap
+    {
+        public string Repo { get; set; }
+        public string Area { get; set; }
+        public string Subarea { get; set; }
+    }
+}
